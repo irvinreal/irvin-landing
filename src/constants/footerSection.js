bbd61ad2a57bc7d2634f56e825a3bb7a1bf776b1
irvinreal <irvin.glez.real@hotmail.com>
2024-01-06 12:35:24 -0600

    arreglando error de carga de imagenes /irvin-landing/...

--- src/constants/footerSection.js
+++ src/constants/footerSection.js
@@ -18,105 +18,105 @@ export const SKILLS = [
     imgId: 'html',
     title: 'HTML 5',
     spanId: 'htmlSpan',
-    img: '/imgs/skillsImgs/html.png',
+    img: '/irvin-landing/imgs/skillsImgs/html.png',
     alt: 'HTML Logo'
   },
   {
     imgId: 'css',
     title: 'CSS 3',
     spanId: 'cssSpan',
-    img: '/imgs/skillsImgs/css.png',
+    img: '/irvin-landing/imgs/skillsImgs/css.png',
     alt: 'CSS Logo'
   },
   {
     imgId: 'javascript',
     title: 'JavaScript',
     spanId: 'javascriptSpan',
-    img: '/imgs/skillsImgs/javascript.png',
+    img: '/irvin-landing/imgs/skillsImgs/javascript.png',
     alt: 'JavaScript Logo'
   },
   {
     imgId: 'tailwindcss',
     title: 'Tailwind CSS',
     spanId: 'tailwindcssSpan',
-    img: '/imgs/skillsImgs/tailwindcss.png',
+    img: '/irvin-landing/imgs/skillsImgs/tailwindcss.png',
     alt: 'Tailwind CSS Logo'
   },
   {
     imgId: 'bootstrap',
     title: 'Bootstrap',
     spanId: 'bootstrapSpan',
-    img: '/imgs/skillsImgs/bootstrap.png',
+    img: '/irvin-landing/imgs/skillsImgs/bootstrap.png',
     alt: 'Bootstrap Logo'
   },
   {
     imgId: 'reactjs',
     title: 'React js',
     spanId: 'reactjsSpan',
-    img: '/imgs/skillsImgs/reactjs.png',
+    img: '/irvin-landing/imgs/skillsImgs/reactjs.png',
     alt: 'Reactjs Logo'
   },
   {
     imgId: 'nextjs',
     title: 'Next js',
     spanId: 'nextjsSpan',
-    img: '/imgs/skillsImgs/nextjs.png',
+    img: '/irvin-landing/imgs/skillsImgs/nextjs.png',
     alt: 'Nextjs Logo'
   },
   {
     imgId: 'git',
     title: 'Git',
     spanId: 'gitSpan',
-    img: '/imgs/skillsImgs/git.png',
+    img: '/irvin-landing/imgs/skillsImgs/git.png',
     alt: 'Git Logo'
   },
   {
     imgId: 'github',
     title: 'Github',
     spanId: 'githubSpan',
-    img: '/imgs/skillsImgs/github.png',
+    img: '/irvin-landing/imgs/skillsImgs/github.png',
     alt: 'Github Logo'
   },
   {
     imgId: 'msql',
     title: 'mySql',
     spanId: 'mysqlSpan',
-    img: '/imgs/skillsImgs/mysql.png',
+    img: '/irvin-landing/imgs/skillsImgs/mysql.png',
     alt: 'MongoDB Logo'
   },
   {
     imgId: 'mongodb',
     title: 'MongoDB',
     spanId: 'mongodbSpan',
-    img: '/imgs/skillsImgs/mongodb.webp',
+    img: '/irvin-landing/imgs/skillsImgs/mongodb.webp',
     alt: 'MongoDB Logo'
   },
   {
     imgId: 'nodejs',
     title: 'nodejs',
     spanId: 'nodejsSpan',
-    img: '/imgs/skillsImgs/nodejs.png',
+    img: '/irvin-landing/imgs/skillsImgs/nodejs.png',
     alt: 'Node js Logo'
   },
   {
     imgId: 'expressjs',
     title: 'expressjs',
     spanId: 'expressjsSpan',
-    img: '/imgs/skillsImgs/expressjs.png',
+    img: '/irvin-landing/imgs/skillsImgs/expressjs.png',
     alt: 'Express js Logo'
   },
   {
     imgId: 'astrojs',
     title: 'astrojs',
     spanId: 'Astro js',
-    img: '/imgs/skillsImgs/astro.png',
+    img: '/irvin-landing/imgs/skillsImgs/astro.png',
     alt: 'Astro js Logo'
   },
   {
     imgId: 'mongoose',
     title: 'mongoose',
     spanId: 'Mongoose',
-    img: '/imgs/skillsImgs/mongoose.png',
+    img: '/irvin-landing/imgs/skillsImgs/mongoose.png',
     alt: 'Mongoose logo'
   }
 ]
