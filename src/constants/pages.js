31fd1a109cdb55ff57dc4b9ad5d88534ca918b33
irvinreal <irvin.glez.real@hotmail.com>
2024-02-05 13:31:02 -0600

    Subtitle especifico para cada pagina en la sección /projects

--- src/constants/pages.js
+++ src/constants/pages.js
@@ -3,36 +3,42 @@ export const PAGES = {
     {
       id: '#porsche',
       title: 'Porsche',
-      url: 'https://porsche-landing-khaki.vercel.app/'
+      url: 'https://porsche-landing-khaki.vercel.app/',
+      subtitle: 'land'
     },
     {
       id: '#tesla',
       title: 'Tesla',
-      url: 'https://tesla-landing-theta.vercel.app/'
+      url: 'https://tesla-landing-theta.vercel.app/',
+      subtitle: 'land'
     },
     {
       id: '#pinterest',
       title: 'Pinteres',
-      url: 'https://pinterest-clone-2022-my-version-2.vercel.app/'
+      url: 'https://pinterest-clone-2022-my-version-2.vercel.app/',
+      subtitle: 'land'
     }
   ],
   games: [
     {
       id: '#snake-game',
       title: 'Snake Game',
-      url: 'https://snake-game-js-virid-delta.vercel.app/'
+      url: 'https://snake-game-js-virid-delta.vercel.app/',
+      subtitle: 'game'
     },
     {
       id: '#tic-tac-toe',
       title: 'Tic tac toe',
-      url: 'https://react-tic-tac-toe-dusky.vercel.app/'
+      url: 'https://react-tic-tac-toe-dusky.vercel.app/',
+      subtitle: 'game'
     }
   ],
   react: [
     {
       id: '#punto-dulce-reposteria',
       title: 'Punto Dulce',
-      url: 'https://punto-dulce.vercel.app/'
+      url: 'https://punto-dulce.vercel.app/',
+      subtitle: 'Next js Project'
     }
   ]
 };
