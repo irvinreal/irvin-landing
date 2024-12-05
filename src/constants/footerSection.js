57be91186c401b1492a14fac5ceb276b61bc4b9b
irvinreal <irvin.glez.real@hotmail.com>
2024-12-05 16:23:21 -0600

    some files deleted from 'src/pages' because of no usage & modified 'src/constants/footerSection.js'

--- src/constants/footerSection.js
+++ src/constants/footerSection.js
@@ -4,13 +4,13 @@ export const MY_PROJECTS = [
   {
     title: 'Tesla Landing Clone',
     img: '/imgs/myProjects/tesla-logo.png',
-    alt: 'Tesla Logo'
+    alt: 'Tesla Logo',
   },
   {
     title: 'Porsche Landing Clone',
     img: '/imgs/myProjects/porsche-logo.png',
-    alt: 'Porsche Logo'
-  }
+    alt: 'Porsche Logo',
+  },
 ]
 
 export const SKILLS = [
@@ -19,104 +19,104 @@ export const SKILLS = [
     title: 'HTML 5',
     spanId: 'htmlSpan',
     img: '/irvin-landing/imgs/skillsImgs/html.png',
-    alt: 'HTML Logo'
+    alt: 'HTML Logo',
   },
   {
     imgId: 'css',
     title: 'CSS 3',
     spanId: 'cssSpan',
     img: '/irvin-landing/imgs/skillsImgs/css.png',
-    alt: 'CSS Logo'
+    alt: 'CSS Logo',
   },
   {
     imgId: 'javascript',
     title: 'JavaScript',
     spanId: 'javascriptSpan',
     img: '/irvin-landing/imgs/skillsImgs/javascript.png',
-    alt: 'JavaScript Logo'
-  },
-  {
-    imgId: 'tailwindcss',
-    title: 'Tailwind CSS',
-    spanId: 'tailwindcssSpan',
-    img: '/irvin-landing/imgs/skillsImgs/tailwindcss.png',
-    alt: 'Tailwind CSS Logo'
-  },
-  {
-    imgId: 'bootstrap',
-    title: 'Bootstrap',
-    spanId: 'bootstrapSpan',
-    img: '/irvin-landing/imgs/skillsImgs/bootstrap.png',
-    alt: 'Bootstrap Logo'
-  },
-  {
-    imgId: 'reactjs',
-    title: 'React js',
-    spanId: 'reactjsSpan',
-    img: '/irvin-landing/imgs/skillsImgs/reactjs.png',
-    alt: 'Reactjs Logo'
-  },
-  {
-    imgId: 'nextjs',
-    title: 'Next js',
-    spanId: 'nextjsSpan',
-    img: '/irvin-landing/imgs/skillsImgs/nextjs.png',
-    alt: 'Nextjs Logo'
-  },
-  {
-    imgId: 'git',
-    title: 'Git',
-    spanId: 'gitSpan',
-    img: '/irvin-landing/imgs/skillsImgs/git.png',
-    alt: 'Git Logo'
-  },
-  {
-    imgId: 'github',
-    title: 'Github',
-    spanId: 'githubSpan',
-    img: '/irvin-landing/imgs/skillsImgs/github.png',
-    alt: 'Github Logo'
-  },
-  {
-    imgId: 'msql',
-    title: 'mySql',
-    spanId: 'mysqlSpan',
-    img: '/irvin-landing/imgs/skillsImgs/mysql.png',
-    alt: 'MongoDB Logo'
-  },
-  {
-    imgId: 'mongodb',
-    title: 'MongoDB',
-    spanId: 'mongodbSpan',
-    img: '/irvin-landing/imgs/skillsImgs/mongodb.webp',
-    alt: 'MongoDB Logo'
-  },
-  {
-    imgId: 'nodejs',
-    title: 'nodejs',
-    spanId: 'nodejsSpan',
-    img: '/irvin-landing/imgs/skillsImgs/nodejs.png',
-    alt: 'Node js Logo'
-  },
-  {
-    imgId: 'expressjs',
-    title: 'expressjs',
-    spanId: 'expressjsSpan',
-    img: '/irvin-landing/imgs/skillsImgs/expressjs.png',
-    alt: 'Express js Logo'
+    alt: 'JavaScript Logo',
   },
   {
     imgId: 'astrojs',
     title: 'astrojs',
     spanId: 'Astro js',
     img: '/irvin-landing/imgs/skillsImgs/astro.png',
-    alt: 'Astro js Logo'
+    alt: 'Astro js Logo',
+  },
+  {
+    imgId: 'reactjs',
+    title: 'React js',
+    spanId: 'reactjsSpan',
+    img: '/irvin-landing/imgs/skillsImgs/reactjs.png',
+    alt: 'Reactjs Logo',
+  },
+  {
+    imgId: 'nextjs',
+    title: 'Next js',
+    spanId: 'nextjsSpan',
+    img: '/irvin-landing/imgs/skillsImgs/nextjs.png',
+    alt: 'Nextjs Logo',
+  },
+  {
+    imgId: 'tailwindcss',
+    title: 'Tailwind CSS',
+    spanId: 'tailwindcssSpan',
+    img: '/irvin-landing/imgs/skillsImgs/tailwindcss.png',
+    alt: 'Tailwind CSS Logo',
+  },
+  {
+    imgId: 'bootstrap',
+    title: 'Bootstrap',
+    spanId: 'bootstrapSpan',
+    img: '/irvin-landing/imgs/skillsImgs/bootstrap.png',
+    alt: 'Bootstrap Logo',
+  },
+  {
+    imgId: 'git',
+    title: 'Git',
+    spanId: 'gitSpan',
+    img: '/irvin-landing/imgs/skillsImgs/git.png',
+    alt: 'Git Logo',
+  },
+  {
+    imgId: 'github',
+    title: 'Github',
+    spanId: 'githubSpan',
+    img: '/irvin-landing/imgs/skillsImgs/github.png',
+    alt: 'Github Logo',
+  },
+  {
+    imgId: 'msql',
+    title: 'mySql',
+    spanId: 'mysqlSpan',
+    img: '/irvin-landing/imgs/skillsImgs/mysql.png',
+    alt: 'MongoDB Logo',
+  },
+  {
+    imgId: 'mongodb',
+    title: 'MongoDB',
+    spanId: 'mongodbSpan',
+    img: '/irvin-landing/imgs/skillsImgs/mongodb.webp',
+    alt: 'MongoDB Logo',
+  },
+  {
+    imgId: 'nodejs',
+    title: 'nodejs',
+    spanId: 'nodejsSpan',
+    img: '/irvin-landing/imgs/skillsImgs/nodejs.png',
+    alt: 'Node js Logo',
+  },
+  {
+    imgId: 'expressjs',
+    title: 'expressjs',
+    spanId: 'expressjsSpan',
+    img: '/irvin-landing/imgs/skillsImgs/expressjs.png',
+    alt: 'Express js Logo',
   },
   {
     imgId: 'mongoose',
     title: 'mongoose',
     spanId: 'Mongoose',
     img: '/irvin-landing/imgs/skillsImgs/mongoose.png',
-    alt: 'Mongoose logo'
-  }
+    alt: 'Mongoose logo',
+  },
 ]
--- src/constants/pages.js
+++ src/constants/pages.js
@@ -4,53 +4,53 @@ export const PAGES = {
       id: '#porsche',
       title: 'Porsche',
       url: 'https://porsche-landing-khaki.vercel.app/',
-      subtitle: 'land'
+      subtitle: 'land',
     },
     {
       id: '#tesla',
       title: 'Tesla',
       url: 'https://tesla-landing-theta.vercel.app/',
-      subtitle: 'land'
+      subtitle: 'land',
     },
     {
       id: '#pinterest',
       title: 'Pinteres',
       url: 'https://pinterest-clone-2022-my-version-2.vercel.app/',
-      subtitle: 'land'
-    }
+      subtitle: 'land',
+    },
   ],
   games: [
     {
       id: '#snake-game',
       title: 'Snake Game',
       url: 'https://snake-game-js-virid-delta.vercel.app/',
-      subtitle: 'game'
+      subtitle: 'game',
     },
     {
       id: '#tic-tac-toe',
       title: 'Tic tac toe',
       url: 'https://react-tic-tac-toe-dusky.vercel.app/',
-      subtitle: 'game'
+      subtitle: 'game',
     },
     {
       id: '#arkanoid',
       title: 'Arkanoid',
       url: 'https://arkanoid-game-p5wib5ktm-irvinreals-projects.vercel.app/',
-      subtitle: 'game'
-    }
+      subtitle: 'game',
+    },
   ],
   react: [
     {
       id: '#punto-dulce-reposteria',
       title: 'Punto Dulce',
       url: 'https://punto-dulce.vercel.app/',
-      subtitle: 'Next js Project'
+      subtitle: 'Next js Project',
     },
     {
       id: '#crud-mern',
       title: 'Crud MERN',
       url: 'https://crud-con-react-nodejs-express-y-mongo-177k4ud29-irvinreal.vercel.app',
-      subtitle: 'Full stack application with Mongo DB, Express, React & Node js'
-    }
-  ]
-};
+      subtitle: 'Full stack application with Mongo DB, Express, React & Node js',
+    },
+  ],
+}
